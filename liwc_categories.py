5395ff88d409601697b52958f9c320f02d08f3a8
Rakib047 <1905047@ugrad.cse.buet.ac.bd>
2024-12-07 16:21:22 +0600

    Modified research code added

---
+++ liwc_categories.py
@@ -0,0 +1,146 @@
+# liwc_categories.py
+
+liwc_categories = {
+    "personal_pronouns": [
+        "i", "me", "my", "mine", "myself", "you", "yours", "yourself", "he", "him",
+        "his", "she", "her", "hers", "it", "its", "itself", "we", "us", "our",
+        "ours", "ourselves", "they", "them", "their", "theirs", "themselves", "who",
+        "whom", "whose", "someone", "somebody", "everyone", "everybody", "anyone",
+        "anybody", "nobody", "another", "one", "each", "either", "neither", "this",
+        "that", "these", "those", "everything", "nothing", "something", "anything",
+        "whoever", "whomever", "where", "wherever", "itself", "ourselves", "theirs",
+        "themselves", "yourself", "yourselves", "we", "us", "our", "ourself",
+        "everyone", "others", "theirself"
+    ],
+    "quantifiers": [
+        "all", "many", "few", "some", "every", "more", "most", "least", "several",
+        "multiple", "much", "numerous", "any", "enough", "each", "either", "both",
+        "half", "double", "triple", "quarter", "none", "plenty", "scant", "minimal",
+        "abundant", "countless", "ample", "adequate", "various", "innumerable",
+        "fewest", "majority", "minority", "sufficient", "entire", "varied", "entirety",
+        "total", "sum", "number", "vast", "bunch", "heaps", "loads", "tons", "handful",
+        "multitude", "plethora", "surplus", "extra", "overabundance", "profuse", "copious"
+    ],
+    "positive_emotion": [
+        "happy", "joy", "love", "glad", "excited", "delight", "pleasure", "satisfaction",
+        "content", "thrilled", "ecstatic", "blissful", "cheerful", "optimistic", "hopeful",
+        "eager", "elated", "proud", "inspired", "grateful", "peaceful", "tranquil", "secure",
+        "confident", "assured", "playful", "funny", "silly", "amusing", "calm", "relaxed",
+        "enthusiastic", "fulfilled", "admiration", "adoration", "affection", "appreciation",
+        "contentment", "excitement", "fascination", "friendliness", "hope", "interest",
+        "jovial", "kindness", "liking", "love", "positivity", "reassurance", "relief",
+        "serenity", "trust", "wonder", "zest", "vivacity", "warmth", "compassion"
+    ],
+    "negative_emotion": [
+        "sad", "angry", "depressed", "upset", "fear", "worried", "anxious", "panic",
+        "afraid", "frustrated", "hopeless", "miserable", "grief", "lonely", "jealous",
+        "resentful", "ashamed", "guilty", "irritated", "annoyed", "bitter", "disappointed",
+        "heartbroken", "overwhelmed", "distressed", "embarrassed", "discouraged", "melancholy",
+        "helpless", "shy", "tense", "angst", "humiliated", "alienated", "isolated", "insecure",
+        "hurt", "rejected", "displeased", "angry", "sorrow", "hostility", "loathing", "pity",
+        "unhappy", "dismayed", "fearful", "agitation", "doubt", "pain", "shame", "defeated"
+    ],
+    "anxiety": [
+        "nervous", "worried", "anxious", "panic", "afraid", "apprehensive", "tense",
+        "uneasy", "restless", "fretful", "insecure", "dread", "fearful", "jittery", "alarmed",
+        "paranoid", "unnerved", "on edge", "distressed", "troubled", "perturbed", "jumpy",
+        "overwhelmed", "unsettled", "agitated", "tense", "disconcerted", "startled",
+        "intimidated", "phobic", "uptight", "panicky", "scared", "doubtful", "shaky",
+        "helpless", "conflicted", "worriedly", "reluctant", "suspicious", "timid",
+        "apprehension", "hesitant", "paranoia", "uncertain", "tormented", "obsessed"
+    ],
+    "death": [
+        "kill", "suicide", "die", "end life", "death", "dead", "deceased", "perish", "murder",
+        "execute", "slaughter", "terminate", "demise", "sacrifice", "lifeless", "corpse",
+        "funeral", "memorial", "cemetery", "graved", "undertaker", "mourn", "burial",
+        "tombstone", "obituary", "coffin", "shroud", "afterlife", "ashes", "reaper",
+        "expire", "euthanasia", "assassinate", "cemetery", "cremation", "interment",
+        "lethal", "martyr", "overdose", "plague", "suicidal", "withered", "grief", "tragedy"
+    ],
+    "swear_words": [
+        "damn", "hell", "f***", "s***", "b****", "a******", "b******", "c***", "d***", "freak",
+        "loser", "jerk", "idiot", "moron", "stupid", "foul", "disgusting", "screwed", "creep",
+        "pervert", "trash", "scum", "crap", "retard", "weirdo", "psycho", "freak", "maniac",
+        "clown", "a******", "tool", "horrible", "awful", "vile", "terrible", "disaster"
+    ],
+    "family": [
+        "family", "father", "mother", "parent", "sibling", "brother", "sister", "uncle",
+        "aunt", "grandfather", "grandmother", "cousin", "relative", "ancestor", "household",
+        "kin", "lineage", "matriarch", "patriarch", "offspring", "child", "children", "dad",
+        "mom", "stepfather", "stepmother", "stepsibling", "grandchild", "great-grandparent",
+        "niece", "nephew", "spouse", "partner", "in-law", "stepbrother", "stepsister", 
+        "adoptive", "descendant", "clan", "dynasty", "heir", "heredity", "progeny", "relations",
+        "relatives", "forebear", "caretaker", "guardian", "dependent", "next of kin", "tribe",
+        "bloodline", "genealogy", "home", "nurture", "generation", "custodian", "legacy"
+    ],
+    "friend": [
+        "friend", "buddy", "mate", "companion", "pal", "ally", "confidant", "comrade",
+        "acquaintance", "bestie", "partner", "teammate", "playmate", "chum", "cohort",
+        "associate", "supporter", "fellow", "sidekick", "classmate", "roommate", "ally",
+        "colleague", "peer", "mentor", "protégé", "buddy", "co-worker", "squad", "gang",
+        "crew", "entourage", "circle", "group", "peer group", "contact", "connection",
+        "friendly", "kindred", "amicable", "amigo", "backup", "guide", "sidekick", "neighbor",
+        "brotherly", "helpful", "supportive", "trustworthy", "loyal"
+    ],
+    "female_references": [
+        "she", "her", "woman", "girl", "lady", "female", "sister", "mother", "daughter",
+        "wife", "aunt", "niece", "queen", "princess", "heroine", "maid", "miss", "ms", 
+        "mrs", "madam", "grandmother", "stepmother", "stepsister", "granddaughter", "matriarch",
+        "actress", "widow", "feminine", "female-led", "femme", "ladyship", "mistress", "nun",
+        "bride", "bachelorette", "hostess", "waitress", "diva", "model", "dame", "womanhood",
+        "maiden", "schoolgirl", "girlhood", "female-identified", "gentlewoman", "matron",
+        "dowager", "virago", "sorority", "girl scout", "motherhood", "daughterhood"
+    ],
+    "male_references": [
+        "he", "his", "man", "boy", "gentleman", "male", "brother", "father", "son", "husband",
+        "uncle", "nephew", "king", "prince", "hero", "lad", "mister", "sir", "mr", "grandfather",
+        "stepfather", "stepbrother", "grandson", "patriarch", "actor", "widower", "masculine",
+        "male-led", "masculinity", "dude", "chap", "guy", "bloke", "husbandry", "lord", "knight",
+        "host", "master", "groom", "bachelor", "monk", "widower", "lad", "schoolboy", "boyhood",
+        "male-identified", "gentlemanly", "matador", "fatherhood", "sonship", "patrician", "ladies' man",
+        "sportsman", "husband-to-be", "manhood", "breadwinner", "warrior", "gentlemen", "cowboy"
+    ],
+    "work": [
+        "work", "job", "career", "profession", "occupation", "employment", "task", "labor",
+        "trade", "craft", "vocation", "calling", "business", "firm", "company", "office", "duty",
+        "responsibility", "role", "gig", "position", "hire", "worker", "employee", "employer",
+        "manager", "supervisor", "colleague", "staff", "team", "co-worker", "enterprise",
+        "freelance", "boss", "paycheck", "wages", "salary", "promotion", "freelancer", "contract",
+        "internship", "trainee", "training", "skilled", "executive", "consultant", "client",
+        "industry", "field", "occupation", "hands-on", "manufacturing", "teamwork", "leadership",
+        "entrepreneur", "self-employed", "full-time", "part-time", "shift"
+    ],
+    "money": [
+        "money", "salary", "income", "wages", "pay", "cash", "wealth", "fortune", "finance",
+        "funds", "capital", "savings", "earnings", "budget", "economy", "dollars", "euros",
+        "pounds", "investment", "stocks", "market", "wealthy", "rich", "affluent", "broke",
+        "poor", "bank", "banking", "loans", "interest", "credit", "debt", "mortgage", "taxes",
+        "account", "savings", "profits", "dividends", "funding", "cash flow", "currency",
+        "fees", "charges", "bills", "wealth management", "expenses", "costs", "savings account",
+        "ATM", "spending", "luxury", "bargain", "afford", "economic", "financial", "gross income"
+    ],
+    "past_focus": [
+        "was", "did", "had", "been", "used", "went", "thought", "knew", "felt", "remembered",
+        "said", "tried", "saw", "heard", "came", "gave", "got", "wanted", "needed", "meant",
+        "took", "told", "learned", "found", "left", "spoke", "wrote", "kept", "sought", "built",
+        "stood", "held", "drew", "ran", "forgot", "loved", "hated", "created", "changed",
+        "lost", "missed", "knew", "explained", "believed", "shared", "suffered", "dreamed",
+        "achieved", "understood", "owned", "wished", "completed", "remembering", "occurred",
+        "remained", "celebrated", "danced", "cried", "laughed"
+    ],
+    "present_focus": [
+        "is", "are", "am", "do", "have", "does", "has", "can", "will", "may", "need", "want",
+        "think", "feel", "know", "like", "mean", "say", "see", "hear", "come", "give", "go",
+        "make", "use", "keep", "try", "take", "ask", "look", "live", "believe", "continue",
+        "work", "play", "learn", "talk", "write", "build", "understand", "speak", "choose",
+        "lead", "ask", "explain", "create", "experience", "find", "watch", "talking"
+    ],
+    "future_focus": [
+        "will", "shall", "going to", "plan", "hope", "expect", "intend", "might", "would",
+        "could", "may", "dream", "aim", "aspire", "imagine", "anticipate", "predict", "wish",
+        "strive", "prepare", "foresee", "decide", "develop", "explore", "try", "seek",
+        "advance", "progress", "expand", "build", "design", "discover", "achieve", "attain",
+        "commit", "invest", "change", "plan for", "set goals", "future", "eventually",
+        "someday", "long-term", "sooner", "afterward", "later"
+    ]
+}
